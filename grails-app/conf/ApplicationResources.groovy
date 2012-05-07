855cfd345744226db9783333c76a8370d86412a3
Nick Maxwell <nickmaxwell10@gmail.com>
2012-05-06 23:41:53 -0400

    resources plugin added.

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,13 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+	
+	core {
+        dependsOn 'jquery, blueprint'
+        resource url:'/js/application.js', disposition: 'head'
+        resource url:'/css/errors.css' 
+        resource url:'/css/main.css'
+        resource url:'/css/mobile.css'
+    }
+	
 }
